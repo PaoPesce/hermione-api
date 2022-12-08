1f427173aec91ed7f4c1191983e1243aca09aff7
paola pesce <paojpesce@gmail.com>
2022-12-07 21:16:28 -0300

    api

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,30 +1,43 @@
 class CharactersController < ApplicationController
+  before_action :set_character, only: %i[show update destroy]
+
   def index
-    @character = Character.all
+    characters = Character.all
+    render json: characters.to_json(only: [:id, :name, :actor, :image_url]), status: 200
   end
 
   def show
-    @character = Characters.find(params[:id])
+    if @character
+      render json: @character
+    else
+      render json: {status: 404, message: 'character not found'}, status: 404
+    end
   end
 
   def create
-    @character = Character.new(params[:character])
-    @character.save
+    character = Character.new(characters_params)
+    if character.save
+
+    else
+
+    end
   end
 
   def update
-    @character = Character.find(params[:id])
-    @character.update(character_params)
+    character.update(characters_params)
   end
 
   def destroy
-    @character = Character.find(paramns[:id])
-    @character.destroy
+    character.destroy
   end
 
   private
 
   def character_params
     params.require(:character).permit(:name, :review, :actor, :image_url)
+  end
+
+  def set_character
+    @character = Character.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,65 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Destroying characters"
+Character.destroy_all if Rails.env.development?
+
+puts "Creating characters"
+Character.create!(name: "Harry Potter", actor: "Daniel")
+
+Character.create(
+  name: 'Severus Snape',
+  review: 'Profesor de Artes oscuras. En el pasado fue aliado y fiel a Valdemort. Estudió con los padres de Harry.',
+  actor: 'Alan Rickman',
+  image_url: 'https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/media/imagenes/reportajes/los-personajes-clave-de-harry-potter/severus-snape_/4661809-1-esl-ES/Severus-Snape_.jpg?resize=980:*'
+)
+
+Character.create(
+  name: 'Minerva McGonagall',
+  review: 'Profesora de Transfiguración y Tutora de la Casa Gryffindor. Estricta pero justa. Una habitual de la saga.',
+  actor: 'Maggie Smith',
+  image_url: 'https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/media/imagenes/reportajes/los-personajes-clave-de-harry-potter/minerva-mcgonagall_/4661789-1-esl-ES/Minerva-McGonagall_.jpg?resize=980:*'
+)
+
+Character.create(
+  name: 'Draco Malfoy',
+  review: 'Procede de una rica familia de magos con querencia por las artes oscuras.',
+  actor: 'Tom Felton',
+  image_url: 'https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/media/imagenes/reportajes/los-personajes-clave-de-harry-potter/draco-malfoy_/4661784-1-esl-ES/Draco-Malfoy_.jpg?resize=980:*'
+)
+
+Character.create(
+  name: 'Harry Potter',
+  review: 'El elegido. A los 11 descubre que es mago y que estudiará en Hogwarts.',
+  actor: 'Daniel Radcliffe',
+  image_url: 'https://res.cloudinary.com/abdulaba/image/upload/v1669900365/Harry/Harry-Potter__htwqje.jpg'
+)
+
+Character.create(
+  name: 'Ron Weasley',
+  review: 'El sexto de los siete hermanos Weasley. Empieza en Hogwarts a la vez que Harry.',
+  actor: 'Rupert Grint',
+  image_url: 'https://res.cloudinary.com/abdulaba/image/upload/v1669900365/Harry/Harry-Potter__htwqje.jpg'
+)
+
+Character.create(
+  name: 'Hermione Granger',
+  review: 'La eterna amiga de Harry y Ron, hija de muggles y la mejor estudiante de Hogwarts.',
+  actor: 'Emma Watson',
+  image_url: 'https://res.cloudinary.com/abdulaba/image/upload/v1669900365/Harry/Harry-Potter__htwqje.jpg'
+)
+
+Character.create(
+  name: 'Rubeus Hagrid',
+  review: 'Enorme guardabosques de Hogwarts. Siente debilidad por los animales exoticos y temibles.',
+  actor: 'Robbie Coltrane',
+  image_url: 'https://res.cloudinary.com/abdulaba/image/upload/v1669900365/Harry/Harry-Potter__htwqje.jpg'
+)
+
+Character.create(
+  name: 'Albus Dumbledore',
+  review: 'Director de Hogwarts. El mago más poderoso que ha existido. Es como un padre para Harry Potter',
+  actor: 'Richard Harris',
+  image_url: 'https://res.cloudinary.com/abdulaba/image/upload/v1669900365/Harry/Harry-Potter__htwqje.jpg'
+)
+
